4efc93e292f9712d74dda0d5a54c844de5937ff1
paolo-caroni <kenren89@gmail.com>
2017-01-11 15:41:02 +0100

    commit unused definitions

--- src/ps3mca-ps1-driver.h
+++ src/ps3mca-ps1-driver.h
@@ -26,8 +26,8 @@ uint8_t BULK_WRITE_ENDPOINT = 			0x02;	/* bEndpointAddress     0x02  EP 2 OUT (B
 uint8_t BULK_READ_ENDPOINT = 			0x81;	/* bEndpointAddress     0x81  EP 1 IN  (Bulk)*/
 uint8_t bulk_buffer[64];				/* wMaxPacketSize     0x0040  1x 64 bytes*/
 
-uint8_t INTERRUPT_READ_ENDPOINT = 		0x83;	/* bEndpointAddress     0x83  EP 3 IN  (Interrupt)*/
-int INTERRUPT_LENGTH = 				1;	/* wMaxPacketSize     0x0001  1x 1 bytes*/
+/*uint8_t INTERRUPT_READ_ENDPOINT = 		0x83;	/* bEndpointAddress     0x83  EP 3 IN  (Interrupt)*/
+/*int INTERRUPT_LENGTH = 				1;	/* wMaxPacketSize     0x0001  1x 1 bytes*/
 
 /* PS3mca commands*/
 uint8_t PS3MCA_CMD_FIRST = 			0xaa;   /* First command for ps3mca protocol*/
@@ -49,7 +49,7 @@ uint8_t RESPONSE_PS2_CARD =			0x02;   /* This is a PS2 card, unused with this dr
 
 /* ---------------------------------------------PS1 Memory Card definitions----------------------------------------------------------*/
 /* Information for PS1 memory card (SCPH-1020, SCPH-1170 and SCPH-119X)*/
-int PS1CARD_TOTAL_SIZE = 131072;			/* 1024x128=131.072 bytes=131,1 kB=128 KiB=1 Megabit*/
+/*int PS1CARD_TOTAL_SIZE = 131072;			/* 1024x128=131.072 bytes=131,1 kB=128 KiB=1 Megabit*/
 int PS1CARD_FRAME_SIZE = 128;				/* frame is the equivalent of a disk sector=128 bytes*/
 uint16_t PS1CARD_MIN_FRAME = 0x0000;			/* 0000h (0) min value frame*/
 uint16_t PS1CARD_MAX_FRAME = 0x03ff;			/* 03ffh (1023) max value of frame (1024 total frame number but 0 is the first)*/
@@ -58,10 +58,8 @@ uint8_t msb;						/* First two significant digits of the frame value*/
 uint8_t lsb;						/* Last two significant digits of the frame value*/
 uint8_t checksum;					/* Checksum variable*/
 uint8_t ps1_ram_buffer[256];				/* 256 bytes of RAM on chip*/
-/*
-int PS1CARD_BLOCK_SIZE = 8192;				/* single block 1024x8=8192 bytes*/
-/*
-int PS1CARD_MAX_BLOCK = 16;				/* max number of block (however 1 is lost for formatting MC)*/
+/*int PS1CARD_BLOCK_SIZE = 8192;				/* single block 1024x8=8192 bytes*/
+/*int PS1CARD_MAX_BLOCK = 16;				/* max number of block (however 1 is lost for formatting MC)*/
 
 /* -------------------------------------------End of PS1 Memory Card definitions------------------------------------------------------*/
 
@@ -80,6 +78,7 @@ uint8_t PS1CARD_CMD_GET_ID = 			0x53;	/* Send Get ID Command (ASCII "S")*/
 uint8_t PS1CARD_CMD_WRITE = 			0x57;	/* Send Write Command (ASCII "W")*/
 
 /* PocketStation commands list*/
+uint8_t POCKETSTATION_CMD_FUNC03 = 		0x50;	/* Change a FUNC 03h related value (ASCII "P")*/
 uint8_t POCKETSTATION_CMD_GET_ID = 		0x58;	/* Send Get ID PocketStation Command (ASCII "X")*/
 uint8_t POCKETSTATION_CMD_59 = 			0x59;	/* Prepare File Execution with Dir_index, and Parameter (ASCII "Y")*/
 uint8_t POCKETSTATION_CMD_5A = 			0x5a;	/* Get Dir_index, ComFlags, F_SN, Date, and Time (ASCII "Z")*/
@@ -88,7 +87,6 @@ uint8_t POCKETSTATION_CMD_TRANSFER_PSX_PKST = 	0x5c;	/* Execute Function and tra
 uint8_t POCKETSTATION_CMD_DOWNLOAD_NOTIF = 	0x5d;	/* Execute Custom Download Notification Function (ASCII "]")*/
 uint8_t POCKETSTATION_CMD_COMFLAGS_1_3_2 = 	0x5e;	/* Get-and-Send ComFlags.bit1,3,2 (send new, get old) (ASCII "^")*/
 uint8_t POCKETSTATION_CMD_COMFLAGS_0 = 		0x5f;	/* Get-and-Send ComFlags.bit0 (send new, get old) (ASCII "_")*/
-uint8_t POCKETSTATION_CMD_FUNC03 = 		0x50;	/* Change a FUNC 03h related value (ASCII "P")*/
 
 /* PocketStation variable list*/
 uint8_t curr_dir_index_bit0_7;
